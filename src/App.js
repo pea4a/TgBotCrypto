2c4bbec307c2ec72bbc64334ae51e0b15ebf2402
Артур Грушніцький <j3.eldo@gmail.com>
2024-06-09 00:41:04 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Typography, Button } from '@mui/material';
+import { Button } from '@mui/material';
 const tg = window.Telegram.WebApp;
 
 
